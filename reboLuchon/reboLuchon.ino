1f919ff853d6ec4d813f4b448612b03c30b5ad2d
angelumoca21 <angelmoca21@gmail.com>
2022-07-09 07:17:57 -0500

    Update reboLuchon.ino

--- reboLuchon/reboLuchon.ino
+++ reboLuchon/reboLuchon.ino
@@ -74,6 +74,11 @@ void loop  ()
       digitalWrite(IN2, 0);
       digitalWrite(IN3, 1);
       digitalWrite(IN4, 0);
+      delay(150);
+      digitalWrite(IN1, 0);
+      digitalWrite(IN2, 0);
+      digitalWrite(IN3, 0);
+      digitalWrite(IN4, 0);
       Serial.println("ADELANTE");
     }
     else if (CharRBL == 'R')
@@ -82,14 +87,37 @@ void loop  ()
       digitalWrite(IN2, 1);
       digitalWrite(IN3, 0);
       digitalWrite(IN4, 1);
+      delay(150);
+      digitalWrite(IN1, 0);
+      digitalWrite(IN2, 0);
+      digitalWrite(IN3, 0);
+      digitalWrite(IN4, 0);
       Serial.println("ATRAS");
     }
     else if (CharRBL == 'I')
     {
+      digitalWrite(IN1, 1);
+      digitalWrite(IN2, 0);
+      digitalWrite(IN3, 0);
+      digitalWrite(IN4, 0);
+      delay(150);
+      digitalWrite(IN1, 0);
+      digitalWrite(IN2, 0);
+      digitalWrite(IN3, 0);
+      digitalWrite(IN4, 0);
       Serial.println("IZQUIERDA");
     }
     else if (CharRBL == 'D')
     {
+      digitalWrite(IN1, 0);
+      digitalWrite(IN2, 0);
+      digitalWrite(IN3, 1);
+      digitalWrite(IN4, 0);
+      delay(150);
+      digitalWrite(IN1, 0);
+      digitalWrite(IN2, 0);
+      digitalWrite(IN3, 0);
+      digitalWrite(IN4, 0);
       Serial.println("DERECHA");
     }
   }
